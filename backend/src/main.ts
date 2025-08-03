061d8f7cc0bb9a515432e576da5883a14fe28eef
hayatohanaoka <hayato.hanaoka@uzabase.com>
2025-08-04 02:09:38 +0900

    setting cors: frontend -> bff and bff -> backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // CORS設定を追加
+  app.enableCors({
+    origin: ['http://localhost:3001'], // BFFからのリクエストを許可
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3002);
 }
 bootstrap();
